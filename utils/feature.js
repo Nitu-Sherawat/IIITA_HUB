b2792c58962291756b58a0edd89c72b3a0502463
Rohit Pandey <iit2022019@iiita.ac.in>
2024-04-26 13:54:08 +0530

    updates done

--- utils/feature.js
+++ utils/feature.js
@@ -1,24 +1,15 @@
 import jwt from "jsonwebtoken";
 import cors from 'cors';
+export const sendCookie = (user,res,message,statusCode =200)=>{
+    const token =jwt.sign({ _id: user._id},process.env.JWT_SECRET);
 
-export const sendCookie = (user, res, message, statusCode = 200) => {
-    try {
-        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
-
-        res.status(statusCode).cookie("token", token, {
-            httpOnly: true,
-            maxAge: 60 * 60 * 1000,
-            sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
-            secure: process.env.NODE_ENV === "production",
-        }).json({
-            success: true,
-            message: message,
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({
-            success: false,
-            message: "Failed to send token as cookie",
-        });
-    }
-};
+    res.status(statusCode).cookie("token",token,{
+        httpOnly: true,
+        maxAge: 15*60*1000,
+        sameSite: process.env.NODE_ENV==="Development" ? "lax" :"none",
+        secure: process.env.NODE_ENV==="Development" ? false :true,
+    }).json({
+        success: true,
+        message: message,
+    });
+}
